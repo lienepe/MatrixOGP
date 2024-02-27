12a9a74296ebd4d3e22c5f833d65fe060d104569
liene <liene@192.168.0.150>
2024-02-27 18:08:41 +0100

    initieel skelet oefening

---
+++ matrix/src/matrix/Matrix.java
@@ -0,0 +1,32 @@
+package matrix;
+
+
+
+public class Matrix {
+
+	public int getRows() {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	public int getColumns() {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	public double getElement(int row, int column) {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	public int[] getMatrix_RMO() {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	public int[] getMatrix_CMO() {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	public int[] getMatrix_AOA() {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	
+}
