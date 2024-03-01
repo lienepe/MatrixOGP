c7c96019dfe4adb7a52621d0b8f31f17c2d98bae
liene <liene@DESKTOP-5G9TTPV.localdomain>
2024-03-01 15:45:14 +0100

    implementatie 1
    implementatie van de immutable versie waarbij elke instantie opgeslagen
    wordt als een RMO opgeslagen

--- matrix/src/matrix/Matrix.java
+++ matrix/src/matrix/Matrix.java
@@ -5,46 +5,91 @@ package matrix;
  */
 
 public class Matrix {
+
+	private int rows;
+	private int columns;
+	private double[] matrix;  //elementen intern opgeslagen in array RMO
 	
-	/**
-	 * deze methode geeft het aantal rijen van de matrix terug
-	 * @pre this != null
-	 * 
-	 * @post this.getRows == this.rijen
-	 * 
-	 */
+	public Matrix(int rows, int columns, double[] matrix) {
+		this.rows = rows;
+		this.columns = columns;
+		this.matrix = matrix.clone();
+	}
+	
 	public int getRows() {
-		throw new RuntimeException("Not yet implemented");
+		return this.rows;
 	}
 	
 	public int getColumns() {
-		throw new RuntimeException("Not yet implemented");
+		return this.columns;
 	}
+	
+	/*
+	 * 
+	 * @param row
+	 * @param column
+	 * @return
+	 */
 	
 	public double getElement(int row, int column) {
-		throw new RuntimeException("Not yet implemented");
+		int i = row*getColumns()+column;
+		return this.matrix[i];
 	}
 	
-	public int[] getMatrix_RMO() {
-		throw new RuntimeException("Not yet implemented");
+	public double[] getMatrix_RMO() {
+		return this.matrix;
 	}
 	
-	public int[] getMatrix_CMO() {
-		throw new RuntimeException("Not yet implemented");
+	public double[] getMatrix_CMO() {
+		double[] newMatrix = new double[getRows()*getColumns()];
+		int i = 0;
+		for (int c=0; c<getColumns();c++) {
+			for (int r = 0; r < getRows(); r++) {
+				double element = getElement(r,c);
+				newMatrix[i] = element;
+				i += 1;
+			}
+		}
+		return newMatrix;
 	}
 	
-	public int[] getMatrix_AOA() {
-		throw new RuntimeException("Not yet implemented");
+	public double[][] getMatrix_AOA() {
+		double[][] newMatrix = new double[getRows()][getColumns()];
+		for (int r=0; r<getRows();r++) {
+			for (int c = 0; c < getColumns(); c++) {
+				double element = getElement(r,c);
+				newMatrix[r][c] = element;
+			}
+		}
+		return newMatrix;
 	}
 	
 	
 	
-	public Matrix scaled() {
-		throw new RuntimeException("Not yet implemented");
+	public Matrix scaled(int n) {
+		double[] scaledMatrix = new double[getRows()*getColumns()];
+		for (int i = 0; i < getRows()*getColumns(); i++) {
+			double element = n*this.matrix[i];
+			scaledMatrix[i] = element;
+		}
+		Matrix newMatrix = new Matrix(this.rows, this.columns, scaledMatrix);
+		return newMatrix;
+
 	}
 	
 	public Matrix plus(Matrix term) {
-		throw new RuntimeException("Not yet implemented");
+		double[] addedMatrix = new double[getRows()*getColumns()];
+		int i = 0;
+		for (int r=0; r<getRows();r++) {
+			for (int c = 0; c < getColumns(); c++) {
+				double element = this.getElement(r,c)+term.getElement(r, c);
+				addedMatrix[i] = element;
+				i += 1;}
+		}
+		Matrix newMatrix = new Matrix(this.rows, this.columns, addedMatrix);
+		return newMatrix;
 	}
 	
 }
+	
+
--- matrix/src/matrix/MatrixTest.java
+++ matrix/src/matrix/MatrixTest.java
@@ -8,7 +8,32 @@ class MatrixTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		double[] elements = new double[] {1,0,0,0,1,0,0,0,1};
+		Matrix eenheid = new Matrix(3,3,elements);
+		assertEquals(eenheid.getRows(), 3);
+		assertEquals(eenheid.getColumns(),3);
+		assertEquals(eenheid.getElement(2, 2),1);
+		assertEquals(eenheid.getElement(0, 0),1);
+		assertEquals(eenheid.getElement(1,0),0);
+		elements[0] = 2;
+		assertEquals(eenheid.getElement(0, 0),1);
+		assertArrayEquals(eenheid.getMatrix_RMO(),eenheid.getMatrix_CMO());
+		
+		double[] elements2 = new double[] {1,2,3,4,5,6};
+		Matrix test = new Matrix(2,3,elements2);
+		Matrix term = new Matrix(2,3,elements2);
+		assertArrayEquals(test.getMatrix_RMO(), elements2);
+		double[] elements2CMO = new double[] {1,4,2,5,3,6};
+		assertArrayEquals(test.getMatrix_CMO(),elements2CMO);
+		double[][] elements2AOA = new double[][] {{1,2,3},{4,5,6}};
+		assertArrayEquals(elements2AOA,test.getMatrix_AOA());
+		
+		assertEquals(test.scaled(2).getElement(0, 0),2);
+		assertEquals(test.scaled(2).getElement(1, 0),8);
+
+		assertEquals(test.plus(term).getElement(0, 0),2);
+		assertEquals(test.plus(term).getElement(1, 0),8);
+		
 	}
 
 }
